dbb2385c10fba00e0c1e0a36e1195dc5305bf8d7
Kaela Maselli <kmaselli@ucvts.org>
2020-01-13 10:45:39 -0500

    wow!

--- src/exercises.js
+++ src/exercises.js
@@ -3,29 +3,60 @@ function commonEnd(a, b) {
     return false;
   }
 
-
-a.length>=1 && b.length>=1
-
  let firstElement1= a[0]
- let lastElement1= a[]
-
+ let lastElement1= a[a.length - 1]
 
  let firstElement2= b[0]
- let lastElement2= b[]
+ let lastElement2= b[b.length - 1]
 
- if (firstElement1===firstElement2||lastElement1===lastElement2){
+ if (firstElement1===firstElement2 || lastElement1===lastElement2 || a !== undefined && b !== undefined && a.length < 1 && b.length < 1){
    return true;
+ } else {
+   return false;
  }
-  // write your code here
 }
 
-function endsMeet(values, n) {
-  // write your code here
-}
+ function endsMeet(values, n) {
+   // if (!values || values.length === 0) {
+   //   return false;
+   // }
+//slice-- subset of the array
+
+// let first =  ;
+// let last =   ;
+//
+//   if (values !== undefined && values.length < 1 && n > 0 || values >= n.length){
+//     console.log( )
+//
+//   } else {
+//     console.log(values[""])
+//   }
+ }
 
 function difference(numbers) {
-  // write your code here
+  if (!numbers || numbers.length === 0){
+    return false;
+  }
+
+  var largest = numbers[0];
+  var smallest = numbers[0];
+
+  for (var i = 1; i < numbers.length; i++) {
+
+      if (numbers[i] > largest) {
+          largest = numbers[i];
+      }
+      else if(numbers[i] < smallest) {
+          smallest = numbers[i];
+      }
 }
+  if (numbers === undefined && numbers >= 1 && !Number.isNaN(numbers)){
+        return undefined;
+      } else {
+        return largest-smallest;
+      }
+
+  }
 
 function max(number) {
   // write your code here
