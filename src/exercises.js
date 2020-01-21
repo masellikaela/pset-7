a8e325254e7873dcfca9e0c96dafe9972334bbf0
Kaela Maselli <ksmaselli@gmail.com>
2020-01-20 20:11:45 -0500

    what is goin on

--- src/exercises.js
+++ src/exercises.js
@@ -25,58 +25,178 @@ function commonEnd(a, b) {
  }
 
 function difference(numbers) {
-  if (!numbers || Number.isNaN(numbers) || numbers < 1){
-        return undefined;
+  if (numbers == undefined || numbers < 1 || numbers.some(isNaN)) {
+    return undefined;
+  } else {
+
+    let bigInteger = Math.max.apply(null, numbers)
+    let smallInteger = Math.min.apply(null, numbers)
+
+    let difference = bigInteger - smallInteger
+
+    return difference
   }
-   //    var largest = numbers[0];
-   //    var smallest = numbers[0];
-   //
-   //    for (var i = 1; i < numbers.length; i++) {
-   //        if (numbers[i] > largest) {
-   //        largest = numbers[i];
-   //        } else if(numbers[i] < smallest) {
-   //        smallest = numbers[i];
-   //      }
-   // }
-
-   var sorted = numbers.slice().sort(function(a, b) {
-     return a - b;
-   });
-
-   var smallest = sorted[0]
-  var largest  = sorted[sorted.length - 1];
-
-        return (largest-smallest);
-};
-function max(number) {
-  // write your code here
 }
 
 function middle(values) {
-  // write your code here
+  let array = [];
+
+  if (values == undefined) {
+
+    return [];
+
+  } else if (values.length < 3) {
+
+    return [];
+
+  } else if (values.length % 2 == 0) {
+    return [];
+  } else {
+    let veryMiddleElement = values[values.length - (Math.ceil(values.length / 2))]
+    let leftMiddleElement = values[(values.length - 1) - (Math.ceil(values.length / 2))]
+
+    array.push(leftMiddleElement, veryMiddleElement)
+
+    return array;
+  }
 }
 
 function increasing(numbers) {
-  // write your code here
+if (numbers == undefined || numbers == []) {
+    return undefined;
+  } else if (numbers.length < 3 || numbers.some(isNaN)) {
+    return undefined;
+  } else {
+
+    for (let i = 1; i < numbers.length; i++) {
+      if ((numbers[i - 1] + 1) === numbers[i]) {
+        i++
+        if ((numbers[i - 1] + 1) === numbers[i]) {
+          return true;
+          break;
+        } else {
+          continue;
+        }
+      } else {
+        continue;
+      }
+    }
+    return false;
+  }
 }
 
 function everywhere(values, x) {
-  // write your code here
+  if (values == undefined || x === undefined) {
+    return false;
+  } else if (values.length < 1) {
+    return false;
+  } else if (!values.includes(x)) {
+    return false;
+  } else {
+
+      for (let y = values.indexOf(x); y < values.length; y++) {
+        if (values[y] === values[y + 1] && values[y + 1] !== undefined) {
+          return true;
+          break;
+        } else if (x === 0) {
+          return false;
+          break;
+        } else if (values[y] === values[y + 2] && values[y + 2] !== undefined && values[y + 1] !== values[y]) {
+          return true;
+          break;
+        } else if (values[y] === values[y + 3] && values[y + 3] !== undefined && (values[y + 1] && values[y + 2]) !== values[y + 3]) {
+          return true;
+          break;
+        } else {
+          return false;
+          break;
+        }
+      }
+  }
 }
 
 function consecutive(numbers) {
-  // write your code here
+  if (numbers == undefined) {
+    return false;
+  } else if (numbers.length < 3) {
+    return false;
+  } else if (numbers.some(isNaN)) {
+    return false;
+  } else {
+      for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0 && numbers[i + 1] % 2 === 0 && numbers[i + 2] % 2 === 0 && numbers[i + 1] !== undefined && numbers[i + 2] !== undefined) {
+          return true;
+          break;
+        }
+
+        if (numbers[i] % 2 !== 0 && numbers[i + 1] % 2 !== 0 && numbers[i + 2] % 2 !== 0 && numbers[i + 1] !== undefined && numbers[i + 2] !== undefined) {
+          return true;
+          break;
+        } else {
+          continue;
+        }
+      }
+      return false;
+  }
 }
 
 function balance(numbers) {
+ if (numbers == undefined) {
+    return false;
+  } else if (numbers.length < 2) {
+    return false;
+  } else if (numbers.some(isNaN)) {
+    return false;
+  } else {
 
+    if (numbers.length === 2 && numbers[0] === numbers[1]) {
+      return true;
+    }
 
-    console.log("test");
+    if (numbers.length % 2 === 0) {
+      let x = numbers.length / 2
+
+      let firstHalf = numbers.slice(0, x)
+      let secondHalf = numbers.slice(x, numbers.length)
+
+      let firstHalfSum = 0;
+      let secondHalfSum = 0;
+
+      for (let i = 0; i < firstHalf.length; i++) {
+        firstHalfSum = firstHalfSum + firstHalf[i]
+      }
+
+      for (let y = 0; y < secondHalf.length; y++) {
+        secondHalfSum = secondHalfSum + secondHalf[y]
+      }
+
+      if (firstHalfSum === secondHalfSum) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  console.log("test");
 }
 
 function clumps(values) {
-  // write your code here
-}
+if (values == undefined) {
+    return -1;
+  } else {
+      let counter = 0
+
+      for (let i = 0; i < values.length; i++) {
+        if (values[i] === values[i + 1] && values[i] !== values[i - 1]) {
+          counter++
+          if (values[i] === values[i + 1] && values[i] === values[i - 1]) {
+          continue;
+        } 
+      }
+      return counter;
+  }
+}}
 
 /*
  * Exports all functions for use in external grader.js file. Do not modify.
